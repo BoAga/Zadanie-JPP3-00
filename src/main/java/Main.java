0eb142913a95fb240c31b6ea4d8bf147d0c8fa17
Aga <aga@git.pl>
2020-02-23 15:15:13 +0100

    Zwraca lancuch znakow

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
+        String tekst = "fedcba";
+        System.out.println(tekst);
+    }
+
+    public static void tekst(String string) {
+    String tekst = "fedcba";
+    System.out.println(tekst);
 
     }
+
+
+
+
+
+
+
+
+
+
+
+
 }
