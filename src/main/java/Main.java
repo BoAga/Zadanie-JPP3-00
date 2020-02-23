db56eaa672b945ac34904bbb420b563304cb303c
jakub <jakub606@interia.eu>
2020-02-23 15:10:01 +0100

    metodakuba

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println(LancuchCo2("adsad"));
+    }
+
+    public static String LancuchCo2(String string) {
+        for (int i = 1; i <= string.length(); i++) {
+        }
+        return "adsad";
 
     }
 }
